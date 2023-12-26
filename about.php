75c79af3f43d840e8c593796a8d00b119cf6d211
Piratheep Rajaratnam <piratheep@xdoto.io>
2023-12-26 11:34:37 +0530

    first commit

---
+++ about.php
@@ -0,0 +1,263 @@
+<?php include "header.php"; ?>
+	<!-- Start header -->
+	<img src="images/banner_about_us.jpg" width="100%">
+	<!-- End header -->
+	
+	<!-- Start About -->
+	<div class="about-section-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 col-md-6 text-center">
+					<div class="inner-column">
+						<h1>Welcome To <span>NorthBrand resturent</span></h1>
+						<h4>Little Story</h4>
+						<p> Located within the city limits of Jaffna, Sri Lanka, The Thinnai Hotel is perfect for travellers who seek to relax amid traditional Northern hospitality. As a part of the Thinnai Group, we focus on providing an authentic experience of Jaffna’s cultural heritage and natural sceneries to tourists while focusing on sustainability and uplifting the local community. The Thinnai hotel has 39 luxurious suites, delicious food, plenty of open spaces and staff from the local community with warm smiles that are friendly and welcoming. </p>
+						<p> 'North' is the Tamil word for a raised platform or verandah found at the entrance of a house. This aesthetically pleasing architectural element is a part of many old Tamil houses in Jaffna and Southern India. Originally it was meant to be the place where family members would sit and observe the surroundings and talk to passers-by. But it is also the place where gossips and discussions happen, and where people would sit and read or have their afternoon nap. As the name suggests, thinnais are an integral part of the hotel’s architecture. Most of its suites have a North, a private verandah where guests can sit to read, relax, talk or to observe the surroundings.</p>
+						<!--<a class="btn btn-lg btn-circle btn-outline-new-white" href="#"> Comment</a> -->
+					</div>
+				</div>
+				<div class="col-lg-6 col-md-6">
+					<img src="images/images.jpg" style="height: 450px; width: 100%" class="img-fluid">
+				</div>
+				<div class="col-md-12">
+					<div class="inner-pt">
+						<p>Maintaining operational and financial records according to the accepted standards prevailing internationally and locally.
+
+Uplifting the local community by purchasing local products as well as helping them improve their standards where they too can develop as we grow. 
+
+Giving equal opportunity to aspiring individuals from all walks of life to join our ranks by adopting a recruitment and selection policy that ensures all stakeholders are involved in the process and can be confident with the transparency of the outcome. 
+
+Continuously developing objectives and strategies to reduce our carbon footprint and to embrace sustainability, at the same time being aware of the environmental impact to our business operations.
+
+Providing high quality guest services that exceed their expectations through continuous innovation and the provision of authentic local experiences.
+ </p>
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End About -->
+	
+	<!-- Start Menu -->
+	<div class="menu-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="heading-title text-center">
+						<h2>Special Menu</h2>
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting</p>
+					</div>
+				</div>
+			</div>
+				
+			<div class="row inner-menu-box">
+				<div class="col-3">
+					<div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+						<a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">All</a>
+						<a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">KathiyaWadi</a>
+						<a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">RajsThani</</a>
+						<a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Rise</a>
+
+						<a class="nav-link" id="v-pills-tava-tab" data-toggle="pill" href="#v-pills-tava" role="tab" aria-controls="v-pills-tava" aria-selected="false">tava</a>
+					</div>
+				</div>
+				
+				<div class="col-9">
+					<div class="tab-content" id="v-pills-tabContent">
+						<div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
+							<div class="row">
+								<?php include "connect.php";
+								$s = mysqli_query($con,"select * from menu");
+								while($r = mysqli_fetch_array($s))
+								{	
+								?>
+								<div class="col-lg-4 col-md-6 special-grid drinks">
+									<div class="gallery-single fix">
+										<img src="admin/<?php echo $r['image']; ?>" class="img-fluid" alt="Image" style='width: 263px; height:170px;'>
+										<div class="why-text">
+											<h4><?php echo $r['title']; ?></h4>
+											<p><?php echo $r['description']; ?></p>
+											<p>Rs <?php echo $r['price']; ?> /-</p>
+											<h5 align="center">
+									<?php if(isset($_SESSION['uid']))
+									{
+									?>
+											<a href="addcart.php?pid=<?php echo $r['id']; ?>&uid=<?php echo $_SESSION['uid']; ?>&price=<?php echo $r['price']; ?>">Add Cart</a></h5>
+
+									<?php
+									}
+									else
+									{
+									?>
+										<a href="login.php">Add Cart</a>
+									<?php } ?>
+
+										</div>
+									</div>
+								</div>
+								<?php } ?>
+							</div>	
+						</div>
+
+
+						<div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
+							<div class="row">
+
+
+							<?php include "connect.php";
+								$s = mysqli_query($con,"select * from menu where category='kathiyawadi'");
+								while($r = mysqli_fetch_array($s))
+								{	
+								?>
+
+								<div class="col-lg-4 col-md-6 special-grid drinks">
+									<div class="gallery-single fix">
+										<img src="admin/<?php echo $r['image']; ?>" class="img-fluid" alt="Image" style='width: 263px; height:170px;'>
+										<div class="why-text">
+											<h4><?php echo $r['title']; ?></h4>
+											<p><?php echo $r['description']; ?></p>
+											<h5>Rs <?php echo $r['price']; ?> /-</h5>
+											<h5 align="center">
+												<?php if(isset($_SESSION['uid']))
+									{
+									?>
+											<a href="addcart.php?pid=<?php echo $r['id']; ?>&uid=<?php echo $_SESSION['uid']; ?>&price=<?php echo $r['price']; ?>">Add Cart</a></h5>
+
+									<?php
+									}
+									else
+									{
+									?>
+										<a href="login.php">Add Cart</a>
+									<?php } ?>
+
+
+
+
+											</h5>
+										</div>
+									</div>
+								</div>
+								<?php } ?>
+								
+								
+							</div>
+							
+						</div>
+						<div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">
+							<div class="row">
+								
+								<?php include "connect.php";
+								$s = mysqli_query($con,"select * from menu where category='rajsthani'");
+								while($r = mysqli_fetch_array($s))
+								{	
+								?>
+								<div class="col-lg-4 col-md-6 special-grid drinks">
+									<div class="gallery-single fix">
+										<img src="admin/<?php echo $r['image']; ?>" class="img-fluid" alt="Image" style='width: 263px; height:170px;'>
+										<div class="why-text">
+											<h4><?php echo $r['title']; ?></h4>
+											<p><?php echo $r['description']; ?></p>
+											<h5>Rs <?php echo $r['price']; ?> /-</h5>
+											<h5 align="center">
+												<?php if(isset($_SESSION['uid']))
+									{
+									?>
+											<a href="addcart.php?pid=<?php echo $r['id']; ?>&uid=<?php echo $_SESSION['uid']; ?>&price=<?php echo $r['price']; ?>">Add Cart</a></h5>
+
+									<?php
+									}
+									else
+									{
+									?>
+										<a href="login.php">Add Cart</a>
+									<?php } ?>
+
+
+											</h5>
+										</div>
+									</div>
+								</div>
+								<?php } ?>
+							</div>
+						</div>
+						<div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">
+							<div class="row">
+                             <?php include "connect.php";
+								$s = mysqli_query($con,"select * from menu where category='rise'");
+								while($r = mysqli_fetch_array($s))
+								{	
+								?>
+								<div class="col-lg-4 col-md-6 special-grid drinks">
+									<div class="gallery-single fix">
+										<img src="admin/<?php echo $r['image']; ?>" class="img-fluid" alt="Image" style='width: 263px; height:170px;'>
+										<div class="why-text">
+											<h4><?php echo $r['title']; ?></h4>
+											<p><?php echo $r['description']; ?></p>
+											<h5>Rs <?php echo $r['price']; ?> /-</h5>
+											<h5 align="center">
+											 <?php if(isset($_SESSION['uid']))
+									{
+									?>
+											<a href="addcart.php?pid=<?php echo $r['id']; ?>&uid=<?php echo $_SESSION['uid']; ?>&price=<?php echo $r['price']; ?>">Add Cart</a></h5>
+
+									<?php
+									}
+									else
+									{
+									?>
+										<a href="login.php">Add Cart</a>
+									<?php } ?>
+	
+
+											</h5>
+										</div>
+									</div>
+								</div>
+								<?php } ?>     
+						</div>
+					</div>
+					<div class="tab-pane fade" id="v-pills-tava" role="tabpanel" aria-labelledby="v-pills-tava-tab">
+							<div class="row">
+								<?php include "connect.php";
+								$s = mysqli_query($con,"select * from menu where category='tava'");
+								while($r = mysqli_fetch_array($s))
+								{	
+								?>
+								<div class="col-lg-4 col-md-6 special-grid drinks">
+									<div class="gallery-single fix">
+										<img src="admin/<?php echo $r['image']; ?>" class="img-fluid" alt="Image" style='width: 263px; height:170px;'>
+										<div class="why-text">
+											<h4><?php echo $r['title']; ?></h4>
+											<p><?php echo $r['description']; ?></p>
+											<h5>Rs <?php echo $r['price']; ?> /-</h5>
+											<h5 align="center"><?php if(isset($_SESSION['uid']))
+									{
+									?>
+											<a href="addcart.php?pid=<?php echo $r['id']; ?>&uid=<?php echo $_SESSION['uid']; ?>&price=<?php echo $r['price']; ?>">Add Cart</a></h5>
+
+									<?php
+									}
+									else
+									{
+									?>
+										<a href="login.php">Add Cart</a>
+									<?php } ?></h5>
+										</div>
+									</div>
+								</div>
+								<?php } ?>	
+
+								
+							
+							</div>
+						</div>
+				</div>
+			</div>
+		</div>
+	</div>								
+
+	<!-- End Menu -->
+	
+	<?php include "footer.php" ; ?>
